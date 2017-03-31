31cf8ddba1c4bbd8439bf18ab1f383d7d8618f87
Mohit Mathur <mohitpmathur@gmail.com>
2017-03-31 00:50:21 -0400

    Baseline NN Model

--- credit-fraud.py
+++ credit-fraud.py
@@ -4,7 +4,8 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import StratifiedKFold
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.wrappers.scikit_learn import KerasClassifier
@@ -12,7 +13,7 @@ from keras.wrappers.scikit_learn import KerasClassifier
 df = pd.read_csv('data/creditcard.csv')
 
 Y = df['Class'].values
-df.drop(['Time', Class], axis=1, inplace=True)
+df.drop(['Time', 'Class'], axis=1, inplace=True)
 X = df.values
 
 def baseline_model():
@@ -21,7 +22,22 @@ def baseline_model():
 	model.add(Dense(29, input_dim=29, kernel_initializer='normal', activation='relu'))
 	model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
 	# Compile model
-	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['auc'])
+	model.compile(loss='binary_crossentropy', optimizer='adam')
 	return model
 
-X_train, y_train, X_test, y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
+
+estimator = KerasClassifier(build_fn=baseline_model, nb_epoch=120, batch_size=5, verbose=2)
+kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+estimator.fit(X_train, y_train)
+y_pred = estimator.predict(X_test)
+
+fpr, tpr, _ = metrics.roc_curve(y_test, y_pred)
+print "AUC Score:", metrics.auc(fpr, tpr)
+print "\nConfusion Matrix:\n", metrics.confusion_matrix(y_test, y_pred)
+print "\nClassification Report:", metrics.classification_report(y_test, y_pred, target_names=['Normal', 'Fraud'])
+
+# THINGS TO TRY
+# Resample the data to evenly distribute
+# Divide 'Normal' data into 5 separate datasets, and use entire 'Fraud' data for each dataset
+# and build model for each.

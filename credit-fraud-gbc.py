b4a26da9741c90ab166cebb639529b4bf2855de7
Mohit Mathur <mohitpmathur@gmail.com>
2017-04-02 23:04:56 -0400

    Adding undersampled file

--- credit-fraud-gbc.py
+++ credit-fraud-gbc.py
@@ -1,6 +1,7 @@
 # Kaggle competition to detect fraud in credit
 # card transactions
 
+import time
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import StratifiedKFold
@@ -8,6 +9,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn import metrics
+from sklearn.utils import shuffle
 
 
 df = pd.read_csv('data/creditcard.csv')
@@ -15,10 +17,7 @@ df = pd.read_csv('data/creditcard.csv')
 Y = df['Class'].values
 df.drop(['Time', 'Class'], axis=1, inplace=True)
 
-X_train, X_test, y_train, y_test = train_test_split(df, 
-													Y,
-													test_size=0.3,
-													random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(df, Y, test_size=0.3, random_state=42)
 
 params = {
 	'learning_rate': [0.001]
@@ -26,10 +25,13 @@ params = {
 gbc = GradientBoostingClassifier()
 #kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 clf = GridSearchCV(gbc, params, cv=3)
+start = time.time()
 clf.fit(X_train, y_train)
+elapsed = time.time() - start
 y_pred = clf.predict(X_test)
 
 fpr, tpr, _ = metrics.roc_curve(y_test, y_pred)
+print "Time Taken to fit model: ", time.strftime('%H:%M:%S', time.gmtime(elapsed))
 print "AUC Score:", metrics.auc(fpr, tpr)
 print "\nConfusion Matrix:\n", metrics.confusion_matrix(y_test, y_pred)
 print "\nClassification Report:", metrics.classification_report(y_test, y_pred, target_names=['Normal', 'Fraud'])
